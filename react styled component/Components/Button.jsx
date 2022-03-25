eaf9708e7c820d966d07f39da2de62bfa0e4575b
mjsvijit <viratsinghfzd75@gmail.com>
2022-03-25 20:08:37 +0530

    Create Button.jsx

---
+++ react styled component/Components/Button.jsx
@@ -0,0 +1,20 @@
+
+import styled from "styled-components"
+
+const Button = styled.button`
+border:none;
+background-color: transparent;
+
+border-radius: 5px;
+cursor: pointer;
+padding: 10px;
+background-color: black;
+color: white;
+`;
+//  const Button = ({children, onClick}) => {
+//   return <button className='btn' onClick={onClick}>{children}</button>
+    
+  
+// }
+export {Button}
+
