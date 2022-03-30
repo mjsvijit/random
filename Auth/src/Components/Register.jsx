58e983b56748139f0c214e7a2bcea818efc3fe24
mjsvijit <viratsinghfzd75@gmail.com>
2022-03-31 01:31:27 +0530

    Create Register.jsx

---
+++ Auth/src/Components/Register.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Register = () => {
+  return (
+    <div>
+    
+
+
+         <input type="text" placeholder="Name" />
+         <br />
+         <input type="email" placeholder='Email'/>
+         <br />
+         <input type="password" placeholder='Password' />
+         <br />
+         <input type="text" placeholder="user-name" />
+         <br />
+         <input type="number" placeholder='Mobile Number' />
+         <br />
+         <input type="text" placeholder="Description" />
+    
+    
+    </div>
+  )
+}
